728b6c78b58912ef134a01895668016e11e11c90
Donald P. Polansky <zizuar@comcast.net>
2019-03-19 07:24:04 -0500

    Friends object starts for survery results,

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+// Objects for friend data
+var surveyMonkies =[
+{
+    "name" : "Ahmed 'No Longer Exists' Doe",
+    "photo" : "https://i.giphy.com/media/KD0JQHYCaE4o0/giphy.webp",
+    "scores" : [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+];
